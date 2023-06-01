8bc988b2210d1f66df569f2e7a391af3e6726c40
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-06-01 11:59:33 +0300

    Implement form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, FormArray, Form } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-project-second';
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  userForm = this.formBuilder.group({
+    email: [''],
+    password: [''],
+    address: this.formBuilder.group({
+      city: [''],
+      house_number: [0]
+    }),
+    posts: this.formBuilder.array([])
+  });
+
+  getCityControl(): FormControl {
+    return (this.userForm.get('address') as FormGroup).get('city') as FormControl;
+  }
+
+  getHouseNumberControl(): FormControl {
+    return (this.userForm.get('address') as FormGroup).get('house_number') as FormControl;
+  }
+
+  getPostsControls(): FormArray {
+    return this.userForm.get('posts') as FormArray;
+  }
+
+  getPostTitleControl(index: number): FormControl {
+    const postFormControl = (this.userForm.get('posts') as FormArray).at(index);
+    return postFormControl.get('title') as FormControl
+  }
+
+  getPostTextControl(index: number): FormControl {
+    const postFormControl = (this.userForm.get('posts') as FormArray).at(index);
+    return postFormControl.get('text') as FormControl
+  }
 }

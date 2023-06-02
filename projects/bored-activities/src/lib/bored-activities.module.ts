ed29fdbff689a89cffdfdb9660bcb9f280229158
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-06-02 14:59:11 +0300

    Install @solit/lucidus

--- projects/bored-activities/src/lib/bored-activities.module.ts
+++ projects/bored-activities/src/lib/bored-activities.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { BoredActivitiesComponent } from './bored-activities.component';
 
-
+import { LucidusModule } from '@solit/lucidus';
 
 @NgModule({
   declarations: [
     BoredActivitiesComponent
   ],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    LucidusModule.forRoot()
   ],
   exports: [
     BoredActivitiesComponent

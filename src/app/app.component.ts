1d8226c2849bd1cde17b7768d547c9868e099ae7
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-06-02 09:59:10 +0300

    Generate a user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,41 +11,4 @@ import { UserForm } from './models/user-form';
 })
 export class AppComponent {
   title = 'test-project-second';
-
-  UserKeys = UserKeys;
-
-  userForm: FormGroup = UserForm.createForm();
-
-  constructor(
-    private ref: ChangeDetectorRef
-  ) { }
-
-  get posts(): FormArray {
-    return UserForm.getPostsFormArray(this.userForm);
-  }
-
-  ngOnInit(): void {
-    const emailControl = UserForm.getEmailControl(this.userForm);
-    emailControl.valueChanges.subscribe(() => {
-      const passwordControl = UserForm.getPasswordControl(this.userForm);
-      setTimeout(() => passwordControl.setValue('1234567890'));
-    })
-  }
-
-  ngAfterViewInit(): void {
-    this.userForm.setValue({
-      [UserKeys.email]: 'example@example.com',
-      [UserKeys.password]: '12345',
-      [UserKeys.address]: {
-        [UserKeys.addressCity]: 'London',
-        [UserKeys.addressHouseNumber]: 3
-      },
-      [UserKeys.posts]: [
-        {
-          [UserKeys.postTitle]: 'post title 1',
-          [UserKeys.postText]: 'post text 1'
-        }
-      ]
-    });
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,

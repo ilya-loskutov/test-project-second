35d127a303a428bcb48f619850ed19c80479163c
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-06-05 11:20:58 +0300

    Turn the app component to the OnPush strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 import { navigationPaths } from './config/navigationPaths';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'test-project-second';

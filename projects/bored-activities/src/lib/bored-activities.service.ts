934c1e7164485432eb8d4b44f9bdc497e68d29b3
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-06-05 12:17:47 +0300

    Implement functionality of the bored activities service

--- projects/bored-activities/src/lib/bored-activities.service.ts
+++ projects/bored-activities/src/lib/bored-activities.service.ts
@@ -1,34 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, BehaviorSubject, throwError } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, BehaviorSubject, of } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
 
-import { BoredActivity, nullBoredActivity } from './models/bored-activity';
+import { BoredActivity, nullBoredActivity, LoadedActivity } from './models/bored-activity';
 import { apiConfig } from './config/api.config';
 
-@Injectable({
-  providedIn: 'root'
-})
 export class BoredActivitiesService {
 
   constructor(
-    private httpClient: HttpClient
+    private readonly httpClient: HttpClient
   ) { }
 
-  public get currentActivity$(): Observable<BoredActivity> {
-    return this._currentActivity$;
-  }
-
-  private _currentActivity$: BehaviorSubject<BoredActivity> = new BehaviorSubject<BoredActivity>(nullBoredActivity);
-
-  async requestNextActivity(participants: number = 1, price: number = 0): Promise<void> {
-    const nextActivity: BoredActivity = await this.httpClient.get<BoredActivity>(this.formRequestUrl(participants, price))
-      .toPromise();
-    if (Math.random() < 0.5) {
-      throw new Error('Data is not available');
-    }
-    this._currentActivity$.next(nextActivity);
+  public getNextActivity(participants: number = 1, price: number = 0): Observable<LoadedActivity> {
+    return this.httpClient.get<BoredActivity>(this.formRequestUrl(participants, price)).pipe(
+      map((activity: BoredActivity) => {
+        if (Math.random() < 0.5) {
+          throw new Error('Data is not available');
+        }
+        return activity;
+      }),
+      map((activity: BoredActivity) => new LoadedActivity(activity)),
+      catchError((error: any) => of(new LoadedActivity(undefined, error))),
+      startWith(new LoadedActivity())
+    )
   }
 
   private formRequestUrl(participants: number, price: number): string {
--- projects/bored-activities/src/lib/models/bored-activity.ts
+++ projects/bored-activities/src/lib/models/bored-activity.ts
@@ -13,3 +13,28 @@ export const nullBoredActivity: BoredActivity = {
     participants: 0,
     price: 0
 }
+
+export class LoadedActivity {
+    constructor(activity?: BoredActivity, error?: Error) {
+        if (activity) {
+            this.activity = activity;
+        }
+        else {
+            this.error = error;
+        }
+    }
+
+    public readonly activity?: BoredActivity;
+    public readonly error?: Error;
+
+    public get isSuccessful(): boolean {
+        return this.activity !== null;
+    }
+
+    public get isPending(): boolean {
+        return this.activity === null && this.error === null;
+    }
+}
+
+
+

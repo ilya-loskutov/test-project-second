43f49a409ee6af4565f2d0c4450a80c16f2e90a8
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-06-05 10:45:12 +0300

    Remove unneeded modules from the boread activities module

--- projects/bored-activities/src/lib/bored-activities.module.ts
+++ projects/bored-activities/src/lib/bored-activities.module.ts
@@ -1,24 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { BoredActivitiesComponent } from './bored-activities.component';
-
-import { LucidusModule } from '@solit/lucidus';
 
 @NgModule({
   declarations: [
     BoredActivitiesComponent
   ],
   imports: [
-    HttpClientModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    LucidusModule.forRoot()
+    CommonModule
   ],
   exports: [
     BoredActivitiesComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { BoredActivitiesModule } from 'bored-activities';
+import { LucidusModule } from '@solit/lucidus';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +21,10 @@ import { UserComponent } from './components/user/user.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    BoredActivitiesModule
+    BoredActivitiesModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    LucidusModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

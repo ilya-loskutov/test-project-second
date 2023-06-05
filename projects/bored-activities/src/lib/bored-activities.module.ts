d8157326f570f3e6997890934f935a5551744048
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-06-05 09:30:00 +0300

    Fix a bug related to importing the bored activities library

--- projects/bored-activities/src/lib/bored-activities.module.ts
+++ projects/bored-activities/src/lib/bored-activities.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { BoredActivitiesComponent } from './bored-activities.component';
 
@@ -15,6 +16,8 @@ import { LucidusModule } from '@solit/lucidus';
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     LucidusModule.forRoot()
   ],
   exports: [
